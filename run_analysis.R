d9220edc5924b6f914e035c460d4edf25e41d10a
Alex Mishin <mishin1990@gmail.com>
2014-12-21 22:30:09 +0200

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -1,5 +1,5 @@
 ##This script loads, cleans and merges the UCI HAR Dataset
-##from a folder named "UCI HAR Dataset" as seen at
+##from a folder named "UCI HAR Dataset", avaiable online at:
 ##https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip
 
 library(dplyr)
@@ -14,9 +14,9 @@ names(X_train) <- features[,2]
 ## loads and combines the subject and activity id numbers to the training dataset
 
 y_train <- read.table("UCI HAR Dataset\\train\\y_train.txt", 
-                      col.names="activity_id")
+                      col.names="activity_id", colClasses= "factor")
 subject_train <- read.table("UCI HAR Dataset\\train\\subject_train.txt", 
-                            col.names="subject_id")
+                            col.names="subject_id", colClasses= "factor")
 training_data <- cbind(subject_train,y_train,X_train)
 
 ##same process for the test dataset
@@ -26,9 +26,9 @@ X_test <- read.table("UCI HAR Dataset\\test\\X_test.txt",
 names(X_test) <- features[,2]
 
 y_test <- read.table("UCI HAR Dataset\\test\\y_test.txt", 
-                     col.names="activity_id")
+                     col.names="activity_id", colClasses= "factor")
 subject_test <- read.table("UCI HAR Dataset\\test\\subject_test.txt",
-                           col.names="subject_id")
+                           col.names="subject_id", colClasses= "factor")
 test_data <- cbind(subject_test,y_test,X_test)
 
 ##Combines the two datasets together,
@@ -56,8 +56,8 @@ names(mean_std) <- gsub("std","standard deviation", names(mean_std))
 ##Finally creates a second tidy dataset with the avrage measurments
 ##For each activity and each subject, and writes it to a file
 
-grouped_avarage <- mean_std %>% 
+grouped_average <- mean_std %>% 
                         group_by(activity_name, subject_id) %>% 
                               summarise_each(funs(mean))
 
-write.table(grouped_avarage, file="grouped_avarage.txt", row.name=FALSE)
+write.table(grouped_average, file="grouped_average.txt", row.name=FALSE)
